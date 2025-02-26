d6310b00d8f511762980fd07d2184ab08feeb5a6
AMANDA_PAOLA_RAMIREZ_AMAYA <a.22308051281284@cbtis128.edu.mx>
2025-02-26 20:42:19 +0000

    Paola Ramirez

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,96 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconos());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconos extends StatelessWidget {
+  const MisIconos({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Paola Ramirez Mat:22308051281284"),
+          titleTextStyle: const TextStyle(color: Colors.black, fontSize: 17),
+          centerTitle: true,
+          backgroundColor: const Color(0xffbfbbbb),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Centra el contenido verticalmente
+            children: <Widget>[
+              // Primera fila de iconos
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Centra los iconos en la fila
+                children: <Widget>[
+                  IconWithSubtitle(
+                      icon: Icons.music_note,
+                      subtitle: 'Musica',
+                      color: Colors.purple),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  IconWithSubtitle(
+                      icon: Icons.favorite,
+                      subtitle: 'Me gusta',
+                      color: Colors.red),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  IconWithSubtitle(
+                      icon: Icons.thumb_up,
+                      subtitle: 'Like',
+                      color: Colors.blue),
+                ],
+              ),
+              SizedBox(height: 20), // Espacio entre las filas
+              // Segunda fila de iconos
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Centra los iconos en la fila
+                children: <Widget>[
+                  IconWithSubtitle(
+                      icon: Icons.bike_scooter,
+                      subtitle: 'Bicicleta',
+                      color: Color(0xff000000)),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  IconWithSubtitle(
+                      icon: Icons.share,
+                      subtitle: 'Compartir',
+                      color: Colors.green),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  IconWithSubtitle(
+                      icon: Icons.delete,
+                      subtitle: 'Eliminar',
+                      color: Color(0xff4b4b4b)),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
+} // fin clase Mis Iconos
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class IconWithSubtitle extends StatelessWidget {
+  final IconData icon;
+  final String subtitle;
+  final Color color;
+
+  const IconWithSubtitle({
+    required this.icon,
+    required this.subtitle,
+    required this.color,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+    return Column(
+      children: <Widget>[
+        Icon(icon, size: 50.0, color: color), // Icono con color personalizado
+        SizedBox(height: 8), // Espacio entre el icono y el subtítulo
+        Text(subtitle, style: TextStyle(fontSize: 16)), // Subtítulo
+      ],
     );
   }
 }
